29d4349396868d2eaf151ed4bcd9851e8a4afe65
JonathanRAD <jonathanrujel4@gmail.com>
2025-10-21 15:26:27 -0500

    preparacion de estructura del proyecto

---
+++ src/app/shared/tabla-generica/tabla-generica.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-generica',
+  imports: [],
+  templateUrl: './tabla-generica.html',
+  styleUrl: './tabla-generica.scss'
+})
+export class TablaGenerica {
+
+}
